5021f75476991650ad1c2448c8ed94808b0b98d1
ShrabanaG <shrabanagoswami@gmail.com>
2024-01-22 16:40:37 +0530

    feat: add components to home page change in global style

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Navbar />
-        <main>{children}</main>
+        <main className="relative overflow-hidden">{children}</main>
         <Footer />
       </body>
     </html>
